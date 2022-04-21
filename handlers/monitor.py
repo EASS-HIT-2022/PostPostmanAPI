3e10e7f844976f00ab0672354edff2846f40616f
liorAbuhav <lior290999@gmail.com>
2022-04-21 19:33:13 +0300

    full CRUD for monitors object

--- handlers/monitor.py
+++ handlers/monitor.py
@@ -1,14 +1,58 @@
 from const.dbConnection import DbConnection
 from models.monitor import Monitor
+from bson.objectid import ObjectId
 
 class MonitorHandler:
     def __init__(self):
         self.monitors_collection = DbConnection.db['Monitors']
 
-    # def get_all_monitors(self):
-    #     db_response = self.monitors_collection.find()
-    #     return {}
+    def get_all_monitors(self):
+        aggregation_pipeline = [
+            {
+                "$project": {
+                    "_id": {
+                        "$toString": "$_id"
+                    },
+                    "name": 1,
+                    "description": 1,
+                    "collection_data": 1
+            }}
+        ]
+        
+        db_response = self.monitors_collection.aggregate(aggregation_pipeline)
+        return list(db_response)
+    
+    def get_monitor_by_id(self, monitor_id: str):
+        aggregation_pipeline = [
+            {
+                "$match": {
+                    "_id": {
+                        "$eq": ObjectId(monitor_id)
+                    }
+                }
+            },
+            {
+                "$project": {
+                    "_id": {
+                        "$toString": "$_id"
+                    },
+                    "name": 1,
+                    "description": 1,
+                    "collection_data": 1
+            }}
+        ]
+        
+        db_response = self.monitors_collection.aggregate(aggregation_pipeline)
+        return list(db_response)[0]
 
-    # def create_monitor(self, monitor: Monitor):
-    #     db_response = self.monitors_collection.insert_one(monitor)
-    #     return {}
+    def create_monitor(self, monitor: Monitor):
+        db_response = self.monitors_collection.insert_one(monitor.__dict__)
+        return {"acknowledged": db_response.acknowledged, "created_count": db_response.created_count}
+
+    def update_monitor(self, monitor_id: str, monitor: Monitor):
+        db_response = self.monitors_collection.update_one(ObjectId(monitor_id), monitor.__dict__)
+        return {"acknowledged": db_response.acknowledged, "modified_count": db_response.modified_count}
+    
+    def delete_monitor(self, monitor_id: str):
+        db_response = self.monitors_collection.delete_one(ObjectId(monitor_id))
+        return {"acknowledged": db_response.acknowledged, "modified_count": db_response.deleted_count}
--- routers/monitor.py
+++ routers/monitor.py
@@ -12,22 +12,45 @@ class MonitorRouter:
         responses={404: {"description": "Not found"}},
     )
 
-    # @staticmethod
-    # @router.get("")
-    # async def get_all_monitors():
-    #     try:
-    #         return monitor_handler.get_all_monitors()
-    #     except Exception as ex:
-    #         raise HTTPException(status_code=500, detail=ex)
+    @staticmethod
+    @router.get("")
+    async def get_all_monitors():
+        try:
+            return monitor_handler.get_all_monitors()
+        except Exception as ex:
+            raise HTTPException(status_code=500, detail=ex)
         
-    # @staticmethod
-    # @router.put("")
-    # async def create_monitor(monitor: Monitor):
-    #     try:
-    #         return monitor_handler.create_monitor(monitor)
-    #     except Exception as ex:
-    #         raise HTTPException(status_code=500, detail=ex)
+    @staticmethod
+    @router.get("/{monitor_id}")
+    async def get_monitor_by_id(monitor_id):
+        try:
+            return monitor_handler.get_monitor_by_id(monitor_id)
+        except Exception as ex:
+            raise HTTPException(status_code=500, detail=ex)    
+        
+    @staticmethod
+    @router.put("")
+    async def create_monitor(monitor: Monitor):
+        try:
+            return monitor_handler.create_monitor(monitor)
+        except Exception as ex:
+            raise HTTPException(status_code=500, detail=ex)
 
+    @staticmethod
+    @router.post("")
+    async def update_monitor(monitor_id: str, monitor: Monitor):
+        try:
+            return monitor_handler.update_monitor(monitor_id, monitor)
+        except Exception as ex:
+            raise HTTPException(status_code=500, detail=ex)
+        
+    @staticmethod
+    @router.delete("")
+    async def delete_monitor(monitor_id: str):
+        try:
+            return monitor_handler.delete_monitor(monitor_id)
+        except Exception as ex:
+            raise HTTPException(status_code=500, detail=ex)
 
     # @router.get("/{item_id}")
     # async def read_item(item_id: str):
