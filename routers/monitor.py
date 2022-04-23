a07a1d1d8857965a1bc91870fd8d6d9af39e41fb
liorAbuhav <lior290999@gmail.com>
2022-04-24 00:43:29 +0300

    fix bug

--- routers/monitor.py
+++ routers/monitor.py
@@ -51,22 +51,3 @@ class MonitorRouter:
             return monitor_handler.delete_monitor(monitor_id)
         except Exception as ex:
             raise HTTPException(status_code=ex.status_code, detail=ex.detail)
-
-    # @router.get("/{item_id}")
-    # async def read_item(item_id: str):
-    #     if item_id not in fake_items_db:
-    #         raise HTTPException(status_code=404, detail="Item not found")
-    #     return {"name": fake_items_db[item_id]["name"], "item_id": item_id}
-
-
-    # @router.put(
-    #     "/{item_id}",
-    #     tags=["custom"],
-    #     responses={403: {"description": "Operation forbidden"}},
-    # )
-    # async def update_item(item_id: str):
-    #     if item_id != "plumbus":
-    #         raise HTTPException(
-    #             status_code=403, detail="You can only update the item: plumbus"
-    #         )
-    #     return {"item_id": item_id, "name": "The great Plumbus"}
